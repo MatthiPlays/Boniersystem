d1204fe70f9fc64d67fbf34bdae56f1078cd26e7
Matthias Zeindler <zeindler.matthi@gmail.com>
2023-11-02 15:39:28 +0100

    Commit 02.11.23. 15:39

---
+++ app/src/main/java/com/zeindler/boniersystem/util/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zeindler.boniersystem.util;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zeindler.boniersystem.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
