98fc9359573d707901c809a550543f90e02dcbfb
Matthias Zeindler <zeindler.matthi@gmail.com>
2023-11-03 16:39:31 +0100

    Commit 03.11.23. 16:39

--- app/src/main/java/com/zeindler/boniersystem/login/LoginFragment.java
+++ app/src/main/java/com/zeindler/boniersystem/login/LoginFragment.java
@@ -7,12 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.zeindler.boniersystem.R;
+import com.zeindler.boniersystem.veranstaltung.*;
 
 public class LoginFragment extends Fragment {
 
     private View view;
+
+    private Kellner[] kellner;
+
+    private TextView tvTitle;
+    private ScrollView svKellner;
+    private ImageButton btnAddKellner;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -37,6 +47,11 @@ public class LoginFragment extends Fragment {
         //TODO: mit Raspberry PI synchronisieren
         //Synchronisiert sollen hier die Kellner Daten werden.
         //Am besten gibt es eine eigene Klasse die sämtliche Connections handelt, da gibt es dann auch eine Klasse die die Kellner synchronisiert
+
+        //TEST
+        kellner = new Kellner[2];
+        kellner[0]=new Kellner(0,"Katrin");
+        kellner[1]=new Kellner(1,"Markus");
     }
 
     private void init(){
---
+++ app/src/main/java/com/zeindler/boniersystem/veranstaltung/Kellner.java
@@ -0,0 +1,16 @@
+package com.zeindler.boniersystem.veranstaltung;
+
+public class Kellner {
+
+    private int id;
+    private String name;
+
+    public Kellner(){
+
+    }
+    public Kellner(int id, String name){
+        this.id=id;
+        this.name=name;
+    }
+
+}
