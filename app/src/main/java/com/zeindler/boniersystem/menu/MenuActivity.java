27d8b92c85f05ac7b59b91373460d93237bdf3e2
Matthias Zeindler <zeindler.matthi@gmail.com>
2023-11-03 07:23:28 +0100

    Commit 03.11.23. 07:23

--- app/src/main/java/com/zeindler/boniersystem/menu/MenuActivity.java
+++ app/src/main/java/com/zeindler/boniersystem/menu/MenuActivity.java
@@ -1,14 +1,18 @@
 package com.zeindler.boniersystem.menu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.zeindler.boniersystem.R;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
 
@@ -20,6 +24,22 @@ public class MenuActivity extends AppCompatActivity {
     }
     private void init(){
         bottomNavigationView = findViewById(R.id.activity_menu_men_mainMenu);
-        bottomNavigationView.setOnNa
+        bottomNavigationView.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int idTisch = R.id.bottom_navigation_menu_itemTische;
+        int itemId = item.getItemId();
+        Fragment selectedFragment = null;
+        switch(2){
+            case R.id.botto:
+                selectedFragment = new TischFragment();
+                break;
+
+        }
+
+
+        return false;
     }
 }
